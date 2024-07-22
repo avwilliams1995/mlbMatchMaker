8c12ac0779537364fd402a0fdc3c250743f12507
droobun <avwilliams1995@gmail.com>
2024-07-22 16:56:37 -0400

    organized players per game

--- scraper/scrapeFunc.py
+++ scraper/scrapeFunc.py
@@ -6,10 +6,53 @@ headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
 
+
+import requests
+from bs4 import BeautifulSoup
+
+def batter_previous_games(player_url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+    
+    response = requests.get(player_url, headers=headers)
+    if response.status_code != 200:
+        print(f'Failed to retrieve the batter_previous_games url. Status code: {response.status_code}')
+        return {}
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Initialize dictionary to store hits and at-bats for the past two games
+    rows = soup.find_all('tr', class_='Table__TR Table__TR--sm Table__even')
+
+    # Initialize dictionary to store stats for the most recent game
+    game_stats = {}
+
+    # Iterate through each row and its td elements
+    for row in rows:
+        cells = row.find_all('td')
+        # Ensure there are enough cells and the first cell contains a date
+        if len(cells) > 4 and '/' in cells[0].text.strip():
+            game_stats = {
+                "date": cells[0].text.strip(),
+                "at_bats": cells[3].text.strip(),
+                "hits": cells[4].text.strip(),
+            }
+            break  # Exit after getting the most recent game stats
+    
+
+    return game_stats
+
+
+
+    
+
+
+
+
 def scrape_batter_splits(pitcher_url):
     
     splits_url = pitcher_url.replace('/player/_/id/', '/player/batvspitch/_/id/')
-    print('pitcher url ------',splits_url)
     response = requests.get(splits_url, headers=headers)
     if response.status_code != 200:
         print(f'Failed to retrieve the splits page. Status code: {response.status_code}')
@@ -18,27 +61,25 @@ def scrape_batter_splits(pitcher_url):
     soup = BeautifulSoup(response.content, 'html.parser')
 
     breakdown_data = []
-
-    # Extract Breakdown section
-    # breakdown_section = soup.select("tbody.Table__TBODY")
-    # print('breakdown section ------',breakdown_section)
+    
     tbodies = soup.find_all('tbody', class_='Table__TBODY')
-
-   
 
     # Iterate over each 'tr' in the 'tbody'
     for row in tbodies[1].find_all('tr', class_='Table__TR Table__TR--sm Table__even'):
         cells = row.find_all('td')
-        print(cells[9])
+        name = cells[0].text.strip()
 
-        if len(cells) > 1 and cells[9] and cells[0]:
+        if len(cells) == 13 and name != "Totals":
+            player_url = cells[0].find('a')['href']
+            prevStats = batter_previous_games(player_url)
             breakdown_data.append({
             "name": cells[0].text.strip(),
             "hits": cells[2].text.strip(),
             "at_bats": cells[1].text.strip(),
             "2b": cells[3].text.strip(),
             "home_runs": cells[5].text.strip(),
-            "avg": cells[9].text.strip()
+            "avg": cells[9].text.strip(),
+            "prevStats": prevStats
             })
                             
     return breakdown_data
@@ -59,7 +100,7 @@ def scrape_pitcher_splits(pitcher_url, i):
     else:
         location = "home"
     
-    locStr = location + "_era"
+    # locStr = location + "_era"
     
     splits_url = pitcher_url.replace('/player/_/id/', '/player/splits/_/id/')
     
@@ -75,7 +116,7 @@ def scrape_pitcher_splits(pitcher_url, i):
 
     breakdown_data = {
         "era": "0.00",
-        locStr: "0.00"
+        "loc_era": "0.00"
     }
     right_left_data = {
         "vs_right": "0.00",
@@ -130,7 +171,7 @@ def scrape_pitcher_splits(pitcher_url, i):
     final = {
         "name": name,
         "era": breakdown_data['era'],
-        locStr: breakdown_data[locStr],
+        "loc_era": breakdown_data["loc_era"],
         "vs_right": right_left_data['vs_right'],
         "vs_left": right_left_data['vs_left']
     }
@@ -172,7 +213,7 @@ def scrape_urls(urls):
             
             splits_data = scrape_pitcher_splits(pitcher_urls[i], i)
             batter_data = scrape_batter_splits(pitcher_urls[i])
-            splits_data["batter_data"] = batter_data
+            splits_data['batter_data'] = batter_data
             pitcher_data.append(splits_data)
         
         final_data.append({"url": game_url, "pitcher_data": pitcher_data})
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,9 +3,11 @@ from scrapeFunc import scrape_urls
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+import pandas as pd
+
 def find_urls():
     # date = datetime.today.strftime('%Y%m%d')
-    date = "20240722"
+    date = "20240723"
 
     url = f"https://www.espn.com/mlb/scoreboard/_/date/{date}"
 
@@ -50,6 +52,48 @@ def send_data_to_server(data):
 
 if __name__ == '__main__':
     scraped_data = find_urls()
-    print("Final data:")
-    print(scraped_data[0])
+    top_batters = []
+    flattened_data = []
+    for game in scraped_data:
+        game_url = game['url']
+        for pitcher in game['pitcher_data']:
+            pitcher_name = pitcher['name']
+            era = pitcher['era']
+            loc_era = pitcher['loc_era']
+            vs_right = pitcher['vs_right']
+            vs_left = pitcher['vs_left']
+            for batter in pitcher['batter_data']:
+                batter_name = batter['name']
+                hits = batter['hits']
+                at_bats = batter['at_bats']
+                doubles = batter['2b']
+                home_runs = batter['home_runs']
+                avg = batter['avg']
+                prev_date = batter['prevStats']['date']
+                prev_at_bats = batter['prevStats']['at_bats']
+                prev_hits = batter['prevStats']['hits']
+                
+                
+                flattened_data.append({
+                    'game_url': game_url,
+                    'pitcher_name': pitcher_name,
+                    'era': era,
+                    'loc_era': loc_era,
+                    'vs_right': vs_right,
+                    'vs_left': vs_left,
+                    'batter_name': batter_name,
+                    'hits': hits,
+                    'at_bats': at_bats,
+                    'doubles': doubles,
+                    'home_runs': home_runs,
+                    'avg': avg,
+                    'prev_date': prev_date,
+                    'prev_at_bats': prev_at_bats,
+                    'prev_hits': prev_hits
+                })
+
+    print(flattened_data[0])
+
+    print("Data has been exported to baseball_data.csv")
+
     # send_data_to_server(scraped_data)
